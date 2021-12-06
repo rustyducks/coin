bab307d09293df9ea0b442cabb642c0960f40460
guilhembn <gbuisan@laas.fr>
2021-12-06 14:02:51 +0100

    Tests on main

--- src/main.cpp
+++ src/main.cpp
@@ -24,13 +24,13 @@ int main(int, char**) {
 
     rd::PointOriented robotPose({1500., 1000., 0.});
     // rd::Arm arm(serialDucklink);
-    rd::PositionControlParameters robotParams = {
+    const rd::PositionControlParameters robotParams = {
         100.,      // maxLinearAcceleration
         300.,      // maxLinearSpeed
-        0.5,       // maxRotationalAcceleration
-        3.0,       // maxRotationalSpeed
+        1.0,       // maxRotationalAcceleration
+        2.0,       // maxRotationalSpeed
         5.,        // admittedLinearPositionError
-        0.05,      // admittedAnglePositionError
+        0.045,     // admittedAnglePositionError
         40.,       // minLinearSpeed
         M_PI / 8.  // minRotationalSpeed
     };
@@ -98,12 +98,12 @@ int main(int, char**) {
                 std::cout << "Position Control loop missed by: " << dt - 5. << "s" << std::endl;
             }*/
             lastControl = now;
-            // std::cout << dt << std::endl;
+            // std::cout << "dt: " << dt << std::endl;
             // std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
             speedCmd = pp.computeSpeed(robotPose, robotSpeed, dt);
             // std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-            //  double exet = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000000.;
-            //   std::cout << exet << std::endl;
+            // double exet = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000000.;
+            // std::cout << "exec time: " << exet << std::endl;
             udpClientJugglerPlot.sendSpeedJson(speedCmd, "speed_cmd");
 
             // std::cout << robotSpeed << std::endl;
@@ -117,11 +117,11 @@ int main(int, char**) {
         }
         double dtSimu = std::chrono::duration_cast<std::chrono::microseconds>(now - lastSimu).count() / 1000000.;
         if (dtSimu > 0.02) {
-            double vxNoise = robotSpeed.vx() < 40. ? 0. : std::normal_distribution<double>(0.0, 7.0)(generator);
-            double vthetaNoise = std::normal_distribution<double>(0.0, 0.005)(generator);
+            double vxNoise = robotSpeed.vx() < 40. ? 0. : std::normal_distribution<double>(0.0, 1.0)(generator);
+            double vthetaNoise = std::normal_distribution<double>(0.0, 0.001)(generator);
             lastSimu = now;
-            robotSpeed = speedCmd * 0.6 + robotSpeed * 0.4 + rd::Speed(vxNoise, 0, vthetaNoise);
-            robotSpeed = rd::Speed(robotSpeed.vx(), robotSpeed.vy(), std::max(-3.0, std::min(3.0, robotSpeed.vtheta())));
+            robotSpeed = speedCmd * 0.4 + robotSpeed * 0.6 + rd::Speed(vxNoise, 0, vthetaNoise);
+            // robotSpeed = rd::Speed(robotSpeed.vx(), robotSpeed.vy(), std::max(-1.5, std::min(1.5, robotSpeed.vtheta())));
             robotPose += rd::PointOriented((robotSpeed.vx() + 0) * robotPose.theta().cos() * dtSimu - robotSpeed.vy() * robotPose.theta().sin() * dtSimu,
                                            (robotSpeed.vx() + 0) * robotPose.theta().sin() * dtSimu + robotSpeed.vy() * robotPose.theta().cos() * dtSimu,
                                            (robotSpeed.vtheta() + 0) * dtSimu);
