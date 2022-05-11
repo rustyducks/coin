3574b144fddab407e7a2dd69e716ff67c2f7974b
guilhembn <buisanguilhem@gmail.com>
2022-05-11 19:04:24 +0200

    Adds full stack build and install script

--- build_install.sh
+++ build_install.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
+BUILD_TYPE=Debug
+
 set -e
 
 cd ../../ && export RUSTYDUCKS_WS_PATH=$(pwd) && echo $RUSTYDUCKS_WS_PATH
 cd $RUSTYDUCKS_WS_PATH/src/protobuf
 mkdir -p build
 cd build && rm -rf *
-cmake -DCMAKE_INSTALL_PREFIX=$RUSTYDUCKS_WS_PATH -DCMAKE_BUILD_TYPE=Release -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_WITH_ZLIB=OFF ../cmake/
+cmake -DCMAKE_INSTALL_PREFIX=$RUSTYDUCKS_WS_PATH -DCMAKE_BUILD_TYPE=$BUILD_TYPE -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_WITH_ZLIB=OFF ../cmake/
 make -j8
 make install
 
 cd $RUSTYDUCKS_WS_PATH/src/ducklink_cpp
 mkdir -p build && cd build && rm -rf *
-cmake -DCMAKE_INSTALL_PREFIX=$RUSTYDUCKS_WS_PATH -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=$RUSTYDUCKS_WS_PATH -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 make -j8
 make install
 
 cd $RUSTYDUCKS_WS_PATH/src/geometry_tools
 mkdir -p build && cd build && rm -rf *
-cmake -DCMAKE_INSTALL_PREFIX=$RUSTYDUCKS_WS_PATH -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=$RUSTYDUCKS_WS_PATH -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 make -j8
 make install
 
 cd $RUSTYDUCKS_WS_PATH/src/navigation
 mkdir -p build && cd build && rm -rf *
-cmake -DCMAKE_INSTALL_PREFIX=$RUSTYDUCKS_WS_PATH -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=$RUSTYDUCKS_WS_PATH -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 make -j8
 make install
 
 cd $RUSTYDUCKS_WS_PATH/src/coin
 mkdir -p build && cd build && rm -rf *
-cmake -DCMAKE_INSTALL_PREFIX=$RUSTYDUCKS_WS_PATH -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=$RUSTYDUCKS_WS_PATH -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 make -j8
 make install
