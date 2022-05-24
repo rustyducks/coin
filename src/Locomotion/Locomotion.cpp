55d45e699785d21c968c9ad10288734f8d9ec343
guilhembn <buisanguilhem@gmail.com>
2022-05-25 00:05:16 +0200

    Handle stop for dalek

--- src/Locomotion/Locomotion.cpp
+++ src/Locomotion/Locomotion.cpp
@@ -94,13 +94,48 @@ Speed Locomotion::run(const double dt) {
             }
         } break;
 
-        case GO_TO_DIFF:
-            outputSpeed = polarControl_.computeSpeed(robotPose_, lastCommand_, dt, 9999999.);
+        case GO_TO_DIFF: {
+            Speed computedSpeed = polarControl_.computeSpeed(robotPose_, lastCommand_, dt, 9999999.);
             if (polarControl_.isGoalReached()) {
                 outputSpeed = Speed(0., 0., 0.);
                 positionControlType_ = IDLE;
             }
-            break;
+            double trajAngle = std::atan2(computedSpeed.vy(), computedSpeed.vx());
+            double c = std::cos(trajAngle);
+            const double tubeHalfWidth = 200;
+            const double tubeInnerLength = 250.;
+            const double tubeOuterLength = 400.;
+            double correctedLinearSpeed = computedSpeed.linearSpeed();
+            double minX = 99999999.;
+            for (const auto& adv : adversaries_) {
+                // Check for the closest adversary in a tube (-maxY, maxY) in front of the robot
+                Point robot2adv = adv.second.transformIn(robotPose_);
+                Point advInTrajFrame = Point(robot2adv.x() * c, robot2adv.y() * c);
+                if (std::abs(advInTrajFrame.y()) <= tubeHalfWidth) {
+                    if (advInTrajFrame.x() > 0 && advInTrajFrame.x() < minX) {  // Forward
+                        minX = advInTrajFrame.x();
+                    }
+                }
+            }
+            correctedLinearSpeed = correctedLinearSpeed * std::max(0., std::min(1., (minX - tubeInnerLength) / (tubeOuterLength - tubeInnerLength)));
+            if (correctedLinearSpeed < computedSpeed.linearSpeed() * 0.95) {
+                goToPointHolonomic_.slowedDown();
+            }
+            if (minX <= tubeInnerLength * 1.15 && std::abs(computedSpeed.vtheta()) < parameters_.minRotationalSpeed) {
+                if (!robotBlocked_) {
+                    robotBlocked_ = true;
+                    robotBlockedSince_ = std::chrono::steady_clock::now();
+                    std::cout << "[Locomotion] Robot is blocked" << std::endl;
+                }
+                outputSpeed = Speed(0., 0., 0.);
+            } else {
+                if (robotBlocked_ && minX >= tubeInnerLength * 1.20) {
+                    robotBlocked_ = false;
+                    std::cout << "[Locomotion] Robot is free again" << std::endl;
+                }
+                outputSpeed = Speed(correctedLinearSpeed * c, 0, computedSpeed.vtheta());
+            }
+        } break;
 
         default:
             outputSpeed = Speed(0., 0., 0.);
