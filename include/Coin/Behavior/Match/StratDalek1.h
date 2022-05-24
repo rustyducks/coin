071b5a1cc3eb028b3b40866063f18e5a7349968b
guilhembn <buisanguilhem@gmail.com>
2022-05-25 00:05:26 +0200

    Strat purple dalek

--- include/Coin/Behavior/Match/StratDalek1.h
+++ include/Coin/Behavior/Match/StratDalek1.h
@@ -14,15 +14,15 @@ ActionJuggler createStratDalek1(Robot& robot, Table& table) {
     auto approachYellowFirstGreen =
         std::make_shared<GoToDiffAction>("Approach Yellow First Green", sGoToDiffTraj({PointOriented(610., 1250., 0.), false, false}));
     auto approachPurpleFirstGreen =
-        std::make_shared<GoToDiffAction>("Approach Yellow First Green", sGoToDiffTraj({PointOriented(2390., 1250., 0.), false, false}));
+        std::make_shared<GoToDiffAction>("Approach Yellow First Green", sGoToDiffTraj({PointOriented(2390., 1390., M_PI), false, false}));
     preMatch->setOnStartYellow(approachYellowFirstGreen);
     preMatch->setOnStartPurple(approachPurpleFirstGreen);
 
     auto yellowFirstGreen = std::make_shared<TakeHexaAction>(sTakeHexaTraj({PointOriented(650., 1250., 0.), false, false}),
                                                              sTakeHexaTraj({PointOriented(560., 1120., 0.), true, false}), Arm::ARM_1,
                                                              table.getHexaByName("Campsite Yellow Green"));
-    auto purpleFirstGreen = std::make_shared<TakeHexaAction>(sTakeHexaTraj({PointOriented(2350., 1250., 0.), false, false}),
-                                                             sTakeHexaTraj({PointOriented(2440., 1120., 0.), true, false}), Arm::ARM_1,
+    auto purpleFirstGreen = std::make_shared<TakeHexaAction>(sTakeHexaTraj({PointOriented(2340., 1390., M_PI), false, false}),
+                                                             sTakeHexaTraj({PointOriented(2440., 1260., M_PI), true, false}), Arm::ARM_1,
                                                              table.getHexaByName("Campsite Purple Green"));
 
     approachYellowFirstGreen->setOnSuccess(yellowFirstGreen);
@@ -31,18 +31,18 @@ ActionJuggler createStratDalek1(Robot& robot, Table& table) {
     auto yellowFirstRed =
         std::make_shared<TakeHexaAction>(sTakeHexaTraj({PointOriented(730., 1120., 0.), false, false}),
                                          sTakeHexaTraj({PointOriented(580., 1370., 0.), true, false}), Arm::ARM_1, table.getHexaByName("Campsite Yellow Red"));
-    auto purpleFirstRed =
-        std::make_shared<TakeHexaAction>(sTakeHexaTraj({PointOriented(2270., 1120., 0.), false, false}),
-                                         sTakeHexaTraj({PointOriented(2420., 1370., 0.), true, false}), Arm::ARM_1, table.getHexaByName("Campsite Purple Red"));
+    auto purpleFirstRed = std::make_shared<TakeHexaAction>(sTakeHexaTraj({PointOriented(2270., 1260., M_PI), false, false}),
+                                                           sTakeHexaTraj({PointOriented(2420., 1510., M_PI), true, false}), Arm::ARM_1,
+                                                           table.getHexaByName("Campsite Purple Red"));
 
     yellowFirstGreen->setOnSuccess(yellowFirstRed);
     purpleFirstGreen->setOnSuccess(purpleFirstRed);
 
-    auto yellowFirstBlue = std::make_shared<TakeHexaAction>(sTakeHexaTraj({PointOriented(710., 1370., 0.), false, false}),
-                                                            sTakeHexaTraj({PointOriented(600., 1370., M_PI / 2.), true, false}), Arm::ARM_1,
+    auto yellowFirstBlue = std::make_shared<TakeHexaAction>(sTakeHexaTraj({PointOriented(710., 1510., 0.), false, false}),
+                                                            sTakeHexaTraj({PointOriented(600., 1510., M_PI / 2.), true, false}), Arm::ARM_1,
                                                             table.getHexaByName("Campsite Yellow Blue"));
-    auto purpleFirstBlue = std::make_shared<TakeHexaAction>(sTakeHexaTraj({PointOriented(2290., 1370., 0.), false, false}),
-                                                            sTakeHexaTraj({PointOriented(2400., 1370., M_PI / 2), true, false}), Arm::ARM_1,
+    auto purpleFirstBlue = std::make_shared<TakeHexaAction>(sTakeHexaTraj({PointOriented(2270., 1510., M_PI), false, false}),
+                                                            sTakeHexaTraj({PointOriented(2540., 1510., M_PI / 2), true, false}), Arm::ARM_1,
                                                             table.getHexaByName("Campsite Purple Blue"));
 
     yellowFirstRed->setOnSuccess(yellowFirstBlue);
@@ -52,15 +52,15 @@ ActionJuggler createStratDalek1(Robot& robot, Table& table) {
         std::make_shared<DropHexaInGallery>(sDropHexaTraj({PointOriented(600., 1760., M_PI / 2), false, false}),
                                             sDropHexaTraj({PointOriented(600., 1600., M_PI / 2), true, false}), Arm::ARM_1, table.getGallery(0));
     auto dropInPurpleBlue =
-        std::make_shared<DropHexaInGallery>(sDropHexaTraj({PointOriented(2400., 1760., M_PI / 2), false, true}),
-                                            sDropHexaTraj({PointOriented(2400., 1600., M_PI / 2), true, false}), Arm::ARM_1, table.getGallery(5));
+        std::make_shared<DropHexaInGallery>(sDropHexaTraj({PointOriented(2540., 1760., M_PI / 2), false, true}),
+                                            sDropHexaTraj({PointOriented(2540., 1600., M_PI / 2), true, false}), Arm::ARM_1, table.getGallery(5));
     yellowFirstBlue->setOnSuccess(dropInYellowBlue);
     purpleFirstBlue->setOnSuccess(dropInPurpleBlue);
 
     auto goToInFrontOfYellowRed =
         std::make_shared<GoToDiffAction>("Move In Front Gallery Yellow Red", sGoToDiffTraj({PointOriented(1080., 1600., M_PI_2), false, true}));
     auto goToInFrontOfPurpleRed =
-        std::make_shared<GoToDiffAction>("Move In Front Gallery Purple Red", sGoToDiffTraj({PointOriented(1920., 1600., M_PI_2), false, true}));
+        std::make_shared<GoToDiffAction>("Move In Front Gallery Purple Red", sGoToDiffTraj({PointOriented(2060., 1600., M_PI_2), false, true}));
 
     dropInYellowBlue->setOnSuccess(goToInFrontOfYellowRed);
     dropInPurpleBlue->setOnSuccess(goToInFrontOfPurpleRed);
@@ -69,8 +69,8 @@ ActionJuggler createStratDalek1(Robot& robot, Table& table) {
         std::make_shared<DropHexaInGallery>(sDropHexaTraj({PointOriented(1080., 1760., M_PI / 2), false, false}),
                                             sDropHexaTraj({PointOriented(1080., 1600., M_PI / 2), true, false}), Arm::ARM_1, table.getGallery(2));
     auto dropInPurpleRed =
-        std::make_shared<DropHexaInGallery>(sDropHexaTraj({PointOriented(1920., 1760., M_PI / 2), false, true}),
-                                            sDropHexaTraj({PointOriented(1920., 1600., M_PI / 2), true, false}), Arm::ARM_1, table.getGallery(3));
+        std::make_shared<DropHexaInGallery>(sDropHexaTraj({PointOriented(2060., 1760., M_PI / 2), false, true}),
+                                            sDropHexaTraj({PointOriented(2060., 1600., M_PI / 2), true, false}), Arm::ARM_1, table.getGallery(3));
 
     goToInFrontOfYellowRed->setOnSuccess(dropInYellowRed);
     goToInFrontOfPurpleRed->setOnSuccess(dropInPurpleRed);
@@ -78,7 +78,7 @@ ActionJuggler createStratDalek1(Robot& robot, Table& table) {
     auto goToInFrontOfYellowGreen =
         std::make_shared<GoToDiffAction>("Move In Front Gallery Yellow Green", sGoToDiffTraj({PointOriented(840., 1600., M_PI_2), false, true}));
     auto goToInFrontOfPurpleGreen =
-        std::make_shared<GoToDiffAction>("Move In Front Gallery Purple Green", sGoToDiffTraj({PointOriented(1160., 1600., M_PI_2), false, true}));
+        std::make_shared<GoToDiffAction>("Move In Front Gallery Purple Green", sGoToDiffTraj({PointOriented(2300., 1600., M_PI_2), false, true}));
 
     dropInYellowRed->setOnSuccess(goToInFrontOfYellowGreen);
     dropInPurpleRed->setOnSuccess(goToInFrontOfPurpleGreen);
@@ -87,8 +87,8 @@ ActionJuggler createStratDalek1(Robot& robot, Table& table) {
         std::make_shared<DropHexaInGallery>(sDropHexaTraj({PointOriented(840., 1760., M_PI / 2), false, false}),
                                             sDropHexaTraj({PointOriented(840., 1600., M_PI / 2), true, false}), Arm::ARM_1, table.getGallery(1));
     auto dropInPurpleGreen =
-        std::make_shared<DropHexaInGallery>(sDropHexaTraj({PointOriented(1160., 1760., M_PI / 2), false, true}),
-                                            sDropHexaTraj({PointOriented(1160., 1600., M_PI / 2), true, false}), Arm::ARM_1, table.getGallery(4));
+        std::make_shared<DropHexaInGallery>(sDropHexaTraj({PointOriented(2300., 1760., M_PI / 2), false, true}),
+                                            sDropHexaTraj({PointOriented(2300., 1600., M_PI / 2), true, false}), Arm::ARM_1, table.getGallery(4));
 
     goToInFrontOfYellowGreen->setOnSuccess(dropInYellowGreen);
     goToInFrontOfPurpleGreen->setOnSuccess(dropInPurpleGreen);
