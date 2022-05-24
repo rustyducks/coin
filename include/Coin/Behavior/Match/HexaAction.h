0799a6d21fd69355557535d24ce0a1c4d9f3d31e
guilhembn <buisanguilhem@gmail.com>
2022-05-24 23:07:34 +0200

    Fixes Hexa action

--- include/Coin/Behavior/Match/HexaAction.h
+++ include/Coin/Behavior/Match/HexaAction.h
@@ -2,18 +2,26 @@
 #define HEXAACTION_H
 
 #include "Coin/Behavior/Match/Action.h"
-#include "Coin/Behavior/Match/HexaAction.h"
 
 namespace rd {
+
+struct sTakeHexaTraj {
+    PointOriented pt;
+    bool backwards;
+    bool withFirstRotation;
+};
 class TakeHexaAction : public Action {
    public:
-    TakeHexaAction(const PointOrientedPtr goTo, const PointOrientedPtr retract, const Arm::ArmID armId, HexaPtr hexa);
+    TakeHexaAction(const sTakeHexaTraj goTo, const sTakeHexaTraj retract, const Arm::ArmID armId, HexaPtr hexa);
     virtual ActionPtr run(Robot& robot) override;
     virtual void deinit(Robot& robot) override;
     virtual bool isDeinit(Robot& robot) override;
 
     void setOnSuccess(ActionPtr onSuccess) { onSuccess_ = onSuccess; }
     void setOnVacuumError(ActionPtr onUnableToVacuum) { onUnableToVacuum_ = onUnableToVacuum; }
+
+    virtual bool goTo(const PointOriented& robotPose, Robot& robot) override;
+    virtual bool retract(const PointOriented& robotPose, Robot& robot) override;
 
     eIntegrityCheck checkIntegrity() override;
 
@@ -24,6 +32,8 @@ class TakeHexaAction : public Action {
     HexaPtr hexa_;
     ActionPtr onSuccess_;
     ActionPtr onUnableToVacuum_;
+    sTakeHexaTraj goTo_;
+    sTakeHexaTraj retract_;
 };
 }  // namespace rd
 
--- src/Behavior/Match/HexaAction.cpp
+++ src/Behavior/Match/HexaAction.cpp
@@ -1,25 +1,29 @@
 #include "Coin/Behavior/Match/HexaAction.h"
 
 namespace rd {
-TakeHexaAction::TakeHexaAction(const PointOrientedPtr goTo, const PointOrientedPtr retract, Arm::ArmID armid, HexaPtr hexa)
-    : Action("Take_" + hexa->name_, goTo, retract), state_(TakeHexaAction::IDLE), armId_(armid), hexa_(hexa) {}
+TakeHexaAction::TakeHexaAction(const sTakeHexaTraj goTo, const sTakeHexaTraj retract, Arm::ArmID armid, HexaPtr hexa)
+    : Action("Take_" + hexa->name_, nullptr, nullptr), state_(TakeHexaAction::IDLE), armId_(armid), hexa_(hexa), goTo_(goTo), retract_(retract) {}
 
-void TakeHexaAction::deinit(Robot& robot) { robot.stackManager.sendHome(armId_); }
+void TakeHexaAction::deinit(Robot&) {}  // robot.stackManager.sendHome(armId_); }
 
-bool TakeHexaAction::isDeinit(Robot& robot) { return robot.stackManager.getStatus() == ProcedureInput::eStatus::SUCCESS; }
+bool TakeHexaAction::isDeinit(Robot& robot) {
+    return (robot.stackManager.getStatus() == ProcedureInput::eStatus::SUCCESS || robot.stackManager.getStatus() == ProcedureInput::eStatus::RUNNING_FREE);
+}
 
 ActionPtr TakeHexaAction::run(Robot& robot) {
     switch (state_) {
         case TakeHexaAction::IDLE:
             if (hexa_->face_ == Hexa::eFace::ROCKS) {
-                robot.stackManager.sendTurnAndPutOnStack(armId_, robot.stackManager.getStackHeight() - robot.stackManager.BASE_STACK_TO_ARM_HOME_HEIGHT);
+                robot.stackManager.sendTurnAndPutOnStack(armId_, robot.stackManager.getStackHeight() + robot.stackManager.SUCCION_TO_STACK_OFFSET -
+                                                                     robot.stackManager.BASE_STACK_TO_ARM_HOME_HEIGHT);
             } else {
-                robot.stackManager.sendPutOnStack(armId_, robot.stackManager.getStackHeight() - robot.stackManager.BASE_STACK_TO_ARM_HOME_HEIGHT);
+                robot.stackManager.sendPutOnStack(armId_, robot.stackManager.getStackHeight() + +robot.stackManager.SUCCION_TO_STACK_OFFSET -
+                                                              robot.stackManager.BASE_STACK_TO_ARM_HOME_HEIGHT);
             }
             state_ = TakeHexaAction::TAKING;
             break;
         case TakeHexaAction::TAKING:
-            if (robot.stackManager.getStatus() == ProcedureInput::eStatus::SUCCESS) {
+            if (robot.stackManager.getStatus() == ProcedureInput::eStatus::RUNNING_FREE || robot.stackManager.getStatus() == ProcedureInput::eStatus::SUCCESS) {
                 robot.stackManager.pushHexaOnStack(hexa_);
                 hexa_->face_ = Hexa::eFace::TREASURE;
                 state_ = TakeHexaAction::DONE;
@@ -58,6 +62,14 @@ Action::eIntegrityCheck TakeHexaAction::checkIntegrity() {
         onUnableVacuumCheck = onUnableToVacuum_->checkIntegrity();
     }
     return assembleIntegrities(onSuccessCheck, onUnableVacuumCheck);
+}
+bool TakeHexaAction::goTo(const PointOriented&, Robot& robot) {
+    robot.locomotion.goToPointDiff(goTo_.pt, goTo_.backwards, goTo_.withFirstRotation);
+    return true;
+}
+bool TakeHexaAction::retract(const PointOriented&, Robot& robot) {
+    robot.locomotion.goToPointDiff(retract_.pt, retract_.backwards, retract_.withFirstRotation);
+    return true;
 }
 
 }  // namespace rd
